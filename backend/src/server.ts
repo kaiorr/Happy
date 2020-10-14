ea52afceb42f7f3930a1224eb9d829f4511e3d9a
kaiorr <kaio.rocharibeiro@gmail.com>
2020-10-14 07:05:41 -0300

    feat: install typeORM para maior abstraçao possivel do codigo com o bando de dados que sera o sqlite.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,4 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3333);
+
+app.get('/users', (request, response)=> {
+  return response.json({ message: "Funfando" })
+})
